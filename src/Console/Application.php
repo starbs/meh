b16cd9f65a1eb40ed3512f59304e36ad12b1c2f2
Graham Campbell <graham@mineuk.com>
2014-12-26 19:31:38 +0000

    Bumped the version

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -32,7 +32,7 @@ class Application extends AbstractApplication
      *
      * @var string
      */
-    protected $appVersion = '1.0.1';
+    protected $appVersion = '1.0.1-dev';
 
     /**
      * Setup the application.
